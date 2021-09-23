5ce496dcd42e07c44fe83d91481728668e4fa03e
oldodpash <73662029+oldodpash@users.noreply.github.com>
2021-09-24 01:49:11 +0300

    Update main.pas

--- main.pas
+++ main.pas
@@ -3,19 +3,19 @@ Var
 P: integer;
 S, d, a:real;
 
-
 begin
 Writeln('Введите P');
 readln(P);
+if P > 0 then
 begin
-If P>0 then
-a:=(P/4);
-d:=(a*sqrt(2));
-S:=(sqr(a));
+a := P / 4;
+d := a*sqrt(2);
+S := sqr(a);
 writeln('a=',d:8:4);
 writeln('d=',d:8:4);
 writeln('S=',S:8:4);
 writeln('Для завершения нажмите Enter');
+end
 else
 writeln('Ошибка: Введенные вами данные некорректны');
 readln();
